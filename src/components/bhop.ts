00428a50c22b9ee2e591ca22e43434635ac9b97a
David Reed <reedswork@proton.me>
2022-09-10 14:18:34 -0400

    fixes

--- src/components/bhop.ts
+++ src/components/bhop.ts
@@ -4,23 +4,20 @@ import { hookCallbacks, objects } from '../loader';
 
 hookCallbacks.push([
 	() => {
-		const { push } = objects.world!.tmpInpts;
-
 		let lastJump = 0;
 
 		objects.world!.tmpInpts.push = function (inputs) {
-			const result = push.call(this, inputs);
-
-			// freeze on server clock
-			// inputs[inputIndex.speedLimit] = [];
+			const result = Array.prototype.push.call(this, inputs);
 
 			if (config.bhop && inputs[inputIndex.jump]) {
 				inputs[inputIndex.jump] = lastJump;
 				lastJump ^= 1;
-				inputs[inputIndex.crouch] =
-					(objects.localPlayer?.velocity.y || 0) < 0
-						? 1
-						: inputs[inputIndex.crouch];
+				// if crouch isn't already held, override crouch
+				if (!inputs[inputIndex.crouch])
+					inputs[inputIndex.crouch] =
+						(objects.localPlayer?.velocity.y || 0) < 0
+							? 1
+							: inputs[inputIndex.crouch];
 			}
 
 			return result;
